098d3630341a4007aea34b7ca86dc85470a09d2c
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2024-07-10 23:19:14 +0400

    Add keycloak for authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,23 @@ import routes from './routes.js'
 import subscriptions from './subscriptions.js'
 import { salesforceConnection, db } from './utils/config.js'
 import bodyParser from 'body-parser';
+import { auth } from 'express-openid-connect'
+
+
 
 const app = express()
 const port = 3000
+
+// Configure session
+app.set('trust proxy', true)
+app.use(
+  auth({
+    issuerBaseURL: 'http://localhost:8080/realms/loyalty',
+    baseURL: 'http://localhost:3000',
+    clientID: 'account-console',
+    secret: 'LONG_RANDOM_STRING'
+  })
+);
 
 app.use(bodyParser.json())
 
