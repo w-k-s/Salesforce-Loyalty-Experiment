09bfd39c95c13da4fd81c02be6d897f647edc8b6
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2024-08-03 17:23:16 +0400

    Get end-to-end tests working

--- backend/test/test.js
+++ backend/test/test.js
@@ -5,26 +5,26 @@ const composeFilePath = ".";
 const composeFile = "docker-compose.yaml";
 
 const environment = await new DockerComposeEnvironment(composeFilePath, composeFile)
-    .withWaitStrategy("keycloak", Wait.forHealthCheck())
-    //.withWaitStrategy("redis", Wait.forLogMessage("Server initialized"))
-    //.withWaitStrategy("mq", Wait.forLogMessage("Server startup complete;"))
-    .withWaitStrategy("loyalty-api", Wait.forHealthCheck())
     .up();
 
+console.log("Docker compose setup complete")
 
 describe('End-to-End Tests', function () {
 
-    afterAll(async () => {
+    after(async () => {
         await environment.down();
     })
 
     describe('Get Products', function () {
-        it('Whatever', async function () {
+        it('Product List is not empty', async function () {
 
             const response = await fetch("http://localhost:3000/api/v1/product")
-            console.log(await response.json())
 
-            assert.equal([1, 2, 3].indexOf(4), -1);
+            assert.strictEqual(200, response.status)
+
+            const json = await response.json()
+
+            assert.strictEqual(true, json["products"].length > 0)
         });
     });
 });
